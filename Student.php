b13ea43a5f22038e09f61b67831ee672fd7fcd48
Sridhar <sreedharriyer@gmail.com>
2016-08-07 08:51:24 -0700

    Student Class

---
+++ Student.php
@@ -0,0 +1,25 @@
+<?php
+//strict
+class Student {
+
+  private  $firstName;
+  private  $lastName;
+  private  $grade;
+
+  public function __construct(string $firstName, string $lastName, string $grade )
+  {
+      $this->firstName = $firstName;
+      $this->lastName = $lastName;
+      $this->grade = $grade;
+  }
+
+  public function show_student()
+  {
+    print $this->firstName . "\n";
+    print $this->lastName . "\n";
+    print $this->grade . "\n";
+  }
+
+}
+
+ ?>
